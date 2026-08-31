e8e02d5abdf23ddbdbddb0a868b03d4f2ac10f36
kurodai290 <riku.natsu.kurodai@gmail.com>
2026-08-31 21:32:25 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
-// app entry
+const createBtn =
+document.getElementById("createBtn");
+
+const joinBtn =
+document.getElementById("joinBtn");
+
+createBtn.addEventListener("click", () => {
+
+    const roomCode =
+    Math.floor(
+        1000 + Math.random() * 9000
+    );
+
+    alert(
+        "ルームコード：" + roomCode
+    );
+
+});
+
+joinBtn.addEventListener("click", () => {
+
+    const roomCode =
+    prompt(
+        "ルームコードを入力"
+    );
+
+    if(roomCode){
+
+        alert(
+            roomCode +
+            " に参加します"
+        );
+
+    }
+
+});
