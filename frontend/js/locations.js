136c50c7adddb83f1df99607870b0f536ed32adf
kurodai290 <riku.natsu.kurodai@gmail.com>
2026-08-31 21:12:22 +0900

    Create locations.js

---
+++ frontend/js/locations.js
@@ -0,0 +1,62 @@
+const LOCATIONS = [
+  {
+    name: "真富士の里",
+    image: "images.jpg",
+    lat: 35.140742,
+    lng: 138.365656
+  },
+  {
+    name: "駿河屋藤枝店",
+    image: "images (1).jpg",
+    lat: 34.867021,
+    lng: 138.261076
+  },
+  {
+    name: "三保の松原",
+    image: "images (2).jpg",
+    lat: 35.004492,
+    lng: 138.527685
+  },
+  {
+    name: "日本平夢テラス",
+    image: "images (3).jpg",
+    lat: 34.974510,
+    lng: 138.465665
+  },
+  {
+    name: "久能山東照宮",
+    image: "images (4).jpg",
+    lat: 34.963270,
+    lng: 138.466937
+  },
+  {
+    name: "焼津ふぃしゅーな",
+    image: "images (5).jpg",
+    lat: 34.856092,
+    lng: 138.326527
+  },
+  {
+    name: "蓮華寺池公園",
+    image: "images (6).jpg",
+    lat: 34.874625,
+    lng: 138.256677
+  },
+  {
+    name: "清沢道の駅",
+    image: "images (7).jpg",
+    lat: 35.031002,
+    lng: 138.243260
+  },
+  {
+    name: "秋葉山公園",
+    image: "images (8).jpg",
+    lat: 35.037302,
+    lng: 138.481293
+  },
+  {
+    name: "焼津サープラあそびタウン",
+    image: "images (9).jpg",
+    lat: 34.878352,
+    lng: 138.307758
+  }
+];
