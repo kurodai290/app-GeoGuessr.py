99066d5d32fd8459e66eb8880fc1a21916a41ceb
kurodai290 <riku.natsu.kurodai@gmail.com>
2026-08-31 21:29:26 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 
+app.use(express.static(__dirname));
+
 app.get("/", (req, res) => {
-  res.send("チャリゲッサー起動成功！");
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
 const PORT = process.env.PORT || 3000;
