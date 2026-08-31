267591bb07a9076801b06c72b3cb7efe51b48d6a
kurodai290 <riku.natsu.kurodai@gmail.com>
2026-08-31 21:26:46 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,19 +1,12 @@
 const express = require("express");
-const path = require("path");
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
-
-// frontend を公開
-app.use(express.static(path.join(__dirname, "../frontend")));
-
-app.get("/api/status", (req, res) => {
-  res.json({
-    status: "online",
-    game: "チャリゲッサー"
-  });
+app.get("/", (req, res) => {
+  res.send("チャリゲッサー起動成功！");
 });
+
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on ${PORT}`);
